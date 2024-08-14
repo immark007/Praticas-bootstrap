5cca1836d7235ee614549de347f6d529f4d5f085
immark007 <immark5507@gmail.com>
2024-08-13 21:08:01 -0300

    lidando com carroseis

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.title h1',{
+    origin: 'bottom',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.title span',{
+    origin: 'top',
+    duration: 2000,
+    distance: '10%'
+});
